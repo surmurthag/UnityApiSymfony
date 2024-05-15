2ddcf75a896610fb687f2f4cb03b482441a25f3c
surmurthag <surmurthag@outlook.fr>
2024-05-15 11:14:35 +0200

    ApiControllerConnexion-2

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ApiController extends AbstractController
